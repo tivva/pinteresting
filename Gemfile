fa8153a79bc30a7939d5885564203c16eb1d377d
tivva <tiffany.w.cheung@gmail.com>
2015-03-12 21:54:00 +0000

    added jquery masonry

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
+gem 'jquery-turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
 gem 'sdoc', '~> 0.4.0',          group: :doc
@@ -15,6 +16,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin]
 gem 'devise', '~>3.4.1'
 gem 'paperclip', '~> 4.2'
 gem 'aws-sdk', '< 2.0'
+gem 'masonry-rails', '~>0.2.0'
 
 group :development, :test do
 	gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port:3000 }  
   # Required for Heroku
   # Note: 'localhost', port:3000 can be changed to website name
+
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+  # Sets paperclip to upload pictures to Amazon S3
 end
